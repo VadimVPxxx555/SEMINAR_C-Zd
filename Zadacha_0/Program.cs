991383e68211e1e3ff3b4e3c327df49d7b1eb061
Vadim V P <Everestxxx555@yandex.ru>
2022-11-30 21:51:27 +0300

    коммит 2 Zadacha_0

--- Zadacha_0/Program.cs
+++ Zadacha_0/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine ("Введите число");
+int number = int.Parse(Console.ReadLine());
+//Console.WriteLine (number);
+int square = number * number;
+//Console.Write ("квадрат числа = ");
+//Console.WriteLine ("квадрат числа" + number +"=" + square);
+Console.WriteLine ($"квадрат числа {number} = {square}");
